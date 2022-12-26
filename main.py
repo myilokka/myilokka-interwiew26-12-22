4edd002b4fb16a3869dacdb873b908aab9a342a2
myilokka <rumpeldumpel03@gmail.com>
2022-12-26 23:06:02 +0800

    finished task

---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+import time
+import uuid
+
+
+class RandomSignal:
+
+    def __init__(self):
+        self.choice = [1, -1]
+        self.result = None
+        self.available_programs = {}
+
+    def add_program(self, program_id):
+        self.available_programs[program_id] = True
+
+    def update(self):
+        self.result = random.choice(self.choice)
+        for key in self.available_programs:
+            self.available_programs[key] = True
+        return self.result
+
+
+class Program:
+
+    def __init__(self, signal, ):
+        self.r_signal = signal
+        self.program_id = uuid.uuid4()
+        signal.add_program(self.program_id)
+
+    def signal(self):
+        if self.r_signal.available_programs[self.program_id]:
+            self.r_signal.available_programs[self.program_id] = False
+            return self.r_signal.result
+
+        return None
+
+
+if __name__ == "__main__":
+    s = RandomSignal()
+    p1 = Program(signal=s)
+    p2 = Program(signal=s)
+
+    while True:
+        s.update()
+        p1.signal()
+        p2.signal()
+        p1.signal()
+        p2.signal()
+        p2.signal()
+        time.sleep(0.9)
+
